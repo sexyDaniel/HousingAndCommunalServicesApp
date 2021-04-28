edc6c28fa0f284c3bb29f864b6e721244ff7c7ed
Danil <danil.lazarev.90@mail.ru>
2021-04-29 02:50:21 +0300

    Add some components in ClientApp

---
+++ ClientApp/src/components/personalNumber.js
@@ -0,0 +1,18 @@
+﻿import React from "react"
+
+export default class PersonalNumber extends React.Component {
+    render() {
+        return <div>
+            <div className="form shadow">
+                <h2>Введите лицевой счет</h2>
+                <form>
+                    <input maxLength="9" className="form-control" placeholder="Лицевой счет" />
+                    <div id="passwordHelpBlock" className="form-text">
+                        Ваш лицевой счет это девятизначный номер
+                            </div>
+                    <button className="btn btn-primary">Посмотреть</button>
+                </form>
+            </div>
+        </div>
+    }
+}
