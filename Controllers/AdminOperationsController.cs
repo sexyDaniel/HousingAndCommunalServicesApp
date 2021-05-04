272c4268e0fb56ec7088ec7cda4ba0d88b45c997
bookWorm <golosov2215>
2021-05-04 18:31:52 +0300

    add function for exit

---
+++ Controllers/AdminOperationsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GKU_App.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class AdminOperationsController : Controller
+    {
+
+    }
+}
